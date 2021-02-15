62c798116f443793783faea3ba31726dcd11d7f9
Pob <590650@gmail.com>
2021-02-15 14:24:30 +0700

    Sidebar menu & Drawer menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,94 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useState } from 'react'
+import { Layout, Button, Drawer, Menu } from 'antd'
+import { MenuOutlined } from '@ant-design/icons'
 import './App.less'
-import styles from './Pob.module.css'
-import Toy from './Toy'
+
+function SideBar({ menus }: { menus: React.ReactElement }) {
+  return (
+    <Layout.Sider
+      breakpoint="sm"
+      collapsedWidth={0}
+      trigger={null}
+      style={{ overflow: 'auto', height: '100vh', position: 'sticky', left: 0, top: 0 }}
+    >
+      {menus}
+    </Layout.Sider>
+  )
+}
+
+function NavBar({
+  renderMenus,
+}: {
+  renderMenus: (setVisible: React.Dispatch<React.SetStateAction<boolean>>) => React.ReactElement
+}) {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <nav>
+      <Button type="primary" icon={<MenuOutlined />} onClick={() => setVisible(true)} />
+      <Drawer title="Topics" placement="left" onClose={() => setVisible(false)} visible={visible}>
+        {renderMenus(setVisible)}
+      </Drawer>
+      Pobby
+    </nav>
+  )
+}
+
+function TopicMenus({
+  topics,
+  selectedKey,
+  changeSelectedKey,
+}: {
+  topics: string[]
+  selectedKey: string
+  changeSelectedKey: (e: any) => void
+}) {
+  return (
+    <Menu onClick={changeSelectedKey} selectedKeys={[selectedKey]}>
+      {topics.map((topic, index) => {
+        return <Menu.Item key={index}>{topic}</Menu.Item>
+      })}
+    </Menu>
+  )
+}
 
 function App() {
+  const topics = ['First Topic', 'Second Topic', 'Third Topic', '4 Topic', '5 Topic', '6 Topic']
+  const [contentIndex, setContentIndex] = useState(0)
+  const [selectedKey, setSelectedKey] = useState('0')
+  function changeSelectedKey(e: { key: string }) {
+    const { key } = e
+    setSelectedKey(key)
+    setContentIndex(+key)
+  }
   return (
     <div>
-      <Button type="primary">Howdy</Button>
-      <h3 className={styles.pob}>Pob</h3>
-      <Toy />
+      <Layout.Header style={{ backgroundColor: 'white' }}>
+        <NavBar
+          renderMenus={(setVisible) => (
+            <TopicMenus
+              topics={topics}
+              selectedKey={selectedKey}
+              changeSelectedKey={(e) => {
+                setVisible(false)
+                changeSelectedKey(e)
+              }}
+            />
+          )}
+        />
+      </Layout.Header>
+      <Layout>
+        <SideBar
+          menus={
+            <TopicMenus
+              topics={topics}
+              selectedKey={selectedKey}
+              changeSelectedKey={changeSelectedKey}
+            />
+          }
+        />
+        <Layout.Content>{topics[contentIndex]}</Layout.Content>
+      </Layout>
     </div>
   )
 }
--- src/Toy.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Toy() {
-  return <div className="toy">Toyyyy</div>
-}
-
-export default Toy
